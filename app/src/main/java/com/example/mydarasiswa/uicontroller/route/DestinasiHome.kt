21ddc6de4691da0ebfcda036e84b347f7ea7527b
hendr1an <hendriansyahgyan@gmail.com>
2025-12-18 21:10:06 +0700

    membuat DestinasiHome.kt

--- app/src/main/java/com/example/mydarasiswa/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/mydarasiswa/uicontroller/route/DestinasiHome.kt
@@ -4,6 +4,7 @@ package com.example.mydarasiswa.uicontroller.route
 import com.example.MyDaraSiswa.R
 import com.example.mydarasiswa.uicontroller.DestinasiNavigasi
 
+
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
